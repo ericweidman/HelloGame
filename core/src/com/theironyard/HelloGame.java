a35d2f5f7e1f67315227ea71b54d9052acff71f8
ericweidman <ericweidman@gmail.com>
2016-02-18 10:35:14 -0500

    Added input interface.

--- core/src/com/theironyard/HelloGame.java
+++ core/src/com/theironyard/HelloGame.java
@@ -2,6 +2,7 @@ package com.theironyard;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -9,6 +10,8 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class HelloGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	float x, y;
+
 	
 	@Override
 	public void create () {
@@ -18,10 +21,24 @@ public class HelloGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
+		if (Gdx.input.isKeyPressed(Input.Keys.W)){
+			y += 1;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.S)){
+			y -= 1;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.D)){
+			x += 1;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.A)){
+			x -= 1;
+		}
+
+
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, x, y);
 		batch.end();
 	}
 }
